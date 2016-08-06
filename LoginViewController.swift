946df418768bbbaa640f1c94f1dd3c7646bd7d6a
ghotjunwoo <ghotjunwoo@naver.com>
2016-08-06 11:25:35 +0900

    constraints

---
+++ LoginViewController.swift
@@ -0,0 +1,74 @@
+//
+//  LoginViewController.swift
+//  Tiat
+//
+//  Created by 이종승 on 2016. 8. 2..
+//  Copyright © 2016년 JW. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    let okAction = UIAlertAction(title: "확인", style: UIAlertActionStyle.Default){(ACTION) in
+        print("B_T")}
+    
+    func login() {
+        FIRAuth.auth()?.signInWithEmail(emailField.text!, password: passwordField.text!, completion:{
+            user, error in
+            
+            if error  != nil {
+                let incorrectAlert = UIAlertController(title: "실패", message: "이메일 또는 비밀번호가 일치하지 않습니다", preferredStyle: UIAlertControllerStyle.Alert);
+                incorrectAlert.addAction(self.okAction);
+                self.presentViewController(incorrectAlert, animated: true, completion: nil)
+            } else {
+                print("Sucess")
+                self.performSegueWithIdentifier("toMainScreen", sender: self)
+                
+            }
+            
+        })
+    }
+    
+    
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        emailField.endEditing(true)
+        passwordField.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    @IBAction func createAccount(sender: AnyObject) {
+        login()
+    }
+
+    @IBAction func NextField(sender: AnyObject) {
+        passwordField.becomeFirstResponder()
+    }
+    
+    @IBAction func GoPressed(sender: AnyObject) {
+        login()
+    }
+    
+        
+}
---
+++ MeViewController.swift
@@ -0,0 +1,49 @@
+//
+//  FirstViewController.swift
+//  Tiat
+//
+//  Created by JWPC1 on 2016. 8. 1..
+//  Copyright © 2016년 JW. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class FirstViewController: UIViewController {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var pointLabel: UILabel!
+    @IBOutlet var birthDayLabel: UILabel!
+    @IBOutlet var genderLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let user = FIRAuth.auth()?.currentUser {
+            let userRef = FIRDatabase.database().reference().child("users/\(user.uid)")
+            
+            print(userRef.child("name"))
+            userRef.child("name").observeEventType(.Value) { (snap: FIRDataSnapshot) in
+                self.nameLabel.text = snap.value?.description
+            }
+            userRef.child("birthday").observeEventType(.Value) { (snap: FIRDataSnapshot) in
+                self.birthDayLabel.text = snap.value?.description
+            }
+            userRef.child("gender").observeEventType(.Value) { (snap: FIRDataSnapshot) in
+                self.genderLabel.text = snap.value?.description
+            }
+            userRef.child("point").observeEventType(.Value) { (snap: FIRDataSnapshot) in
+                self.pointLabel.text = snap.value?.description
+            }
+            print("Sucessfully Signed in")
+            
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
